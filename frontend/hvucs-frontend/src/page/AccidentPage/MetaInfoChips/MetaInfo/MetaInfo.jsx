b39a02d0c257d485b46c1264da036b1d9fa53ba9
isworldnew <isworldnew@mail.ru>
2026-06-15 01:35:47 +0400

    timezoned render

--- frontend/hvucs-frontend/src/page/AccidentPage/MetaInfoChips/MetaInfo/MetaInfo.jsx
+++ frontend/hvucs-frontend/src/page/AccidentPage/MetaInfoChips/MetaInfo/MetaInfo.jsx
@@ -2,21 +2,36 @@ import './MetaInfo.css';
 
 export default function MetaInfo({ info, infoType }) {
     
+    // const formatDateTime = (dateTimeString) => {
+    //     if (!dateTimeString) return '';
+        
+    //     const date = new Date(dateTimeString);
+        
+    //     const hours = date.getUTCHours().toString().padStart(2, '0');
+    //     const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+    //     const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+    //     const day = date.getUTCDate().toString().padStart(2, '0');
+    //     const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+    //     const year = date.getUTCFullYear();
+        
+    //     return `${hours}:${minutes}:${seconds}, ${day}/${month}/${year}`;
+    // };
+    
     const formatDateTime = (dateTimeString) => {
         if (!dateTimeString) return '';
         
         const date = new Date(dateTimeString);
         
-        const hours = date.getUTCHours().toString().padStart(2, '0');
-        const minutes = date.getUTCMinutes().toString().padStart(2, '0');
-        const seconds = date.getUTCSeconds().toString().padStart(2, '0');
-        const day = date.getUTCDate().toString().padStart(2, '0');
-        const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
-        const year = date.getUTCFullYear();
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        const seconds = date.getSeconds().toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
         
         return `${hours}:${minutes}:${seconds}, ${day}/${month}/${year}`;
     };
-    
+
     const formatAccuracy = (value) => {
         if (value === undefined || value === null) return '';
         const percentage = (value * 100).toFixed(2);
